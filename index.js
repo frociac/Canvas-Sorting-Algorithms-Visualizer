064b160a0ff8d08b0507192c2616ca063806908e
Brad <51890675+frociac@users.noreply.github.com>
2023-08-19 04:05:12 -0400

    Divide code to handler and utils

--- index.js
+++ index.js
@@ -1,106 +1,67 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-ctx.fillStyle = "rgb(0,0,0)"
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-const max = canvas.height;
-const min = 1;
-let stopSort = false;
-let currentlySelected = "bogo";
-let size = 10;
+document.addEventListener("DOMContentLoaded", () => {
+  // starts canvas in utils.js and stores context
+  setUpCanvas();
 
-function generateRandomArray(max, min, size) {
-  const array = [];
-  for (let i = 0; i < size; i++) {
-    const element = Math.floor(Math.random() * (max - min + 1) + min);
-    array.push(element);
-    // const node = document.createElement("div");
-    // const textnode = document.createTextNode(element);
-    // node.appendChild(textnode);
-    // document.body.appendChild(node);
-  }
-  console.log(array);
-  return array;
-}
-async function draw(array, comparePoints=[], sortedPoints=[]) {
-  
-  ctx.fillStyle = "rgb(0,0,0)"
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  const rectWidth = canvas.width/size;
-  if (canvas.getContext) {
-    for(let i = 0; i < array.length; i++) {
-      if (comparePoints.includes(i)) ctx.fillStyle = 'yellow';
-      else if (sortedPoints.includes(i)) ctx.fillStyle = 'green';
-      else ctx.fillStyle = 'white';
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = 'black';
-      ctx.strokeRect(i * rectWidth + ctx.lineWidth / 2, canvas.height - array[i] + ctx.lineWidth / 2, rectWidth - ctx.lineWidth, array[i] - ctx.lineWidth);
-      ctx.fillRect(i * rectWidth + ctx.lineWidth / 2 + 1, canvas.height - array[i] + ctx.lineWidth / 2 + 1, rectWidth - ctx.lineWidth - 1, array[i] - ctx.lineWidth - 1);
+  /** slider handler */
+  const slider = document.getElementById("size");
+  const valueDisplay = document.getElementById("value-display");
+
+  // update the value display when the slider value changes
+  slider.addEventListener("input", () => {
+    valueDisplay.textContent = slider.value;
+    setSize(slider.value);
+  });
+  /** end of slider handler */
+
+  /** sorting buttons handler */
+  const buttonContainer = document.getElementById("left");
+  const startButton = document.getElementById("start");
+  const stopButton = document.getElementById("stop");
+  // adds an event listener to the div containing buttons
+  buttonContainer.addEventListener("click", (event) => {
+    // ensures the button was pressed and not the div
+    let target = event.target;
+    if (target.classList.contains("choice-button")) {
+      startButton.classList.remove("disabled");
+      if (target.id == "random") {
+        target = buttonContainer.children[Math.floor(Math.random() * (buttonContainer.children.length -1))];
+        console.log(target);
+      }
+      sortSelect(target.id);
+      target.classList.add("clicked");
+      // removes clicked class from previously clicked button (makes all other buttons turn red)
+      for (let i = 0; i < buttonContainer.children.length; i++) {
+        let childButton = buttonContainer.children[i];
+        if (childButton.id != target.id) {
+          childButton.classList.remove("clicked");
+        }
+      }
     }
-    await wait(100);
-  }
-}
+  });
+  /** end of sorting buttons handler */
 
-function sort() {
-  let button = document.getElementById("sort");
-  button.onclick = () => {
-    stopSort = true;
-  }
-  button.textContent = "Stop";
-  switch(currentlySelected){
-    case "bogo":
-      bogoSort();
-  } 
-}
-
-function stopExecution() {
-  ctx.fillStyle = "rgb(0,0,0)"
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  let button = document.getElementById("sort");
-  button.onclick = sort;
-  button.textContent = "Sort";
-  stopSort = false;
-}
-
-async function bogoSort() {
-  let array = generateRandomArray(max, min, size);
-  await draw(array);
-  while(!(await isSorted(array))) {
-    if (stopSort) return stopExecution();
-    array = await shuffle(array)
-    await draw(array);
-  }
-  await draw(array, [], Array.from(Array(array.length).keys()))
-  console.log(array);
-}
-
-async function isSorted(array) {
-  for (let i = 1; i < array.length; i++) {
-    await draw(array, [i-1,i])
-    if (array[i] < array[i-1]) {
-      return false;
+  /** input handler */
+  const elementsInput = document.getElementById("elements");
+  const speedInput = document.getElementById("speed");
+  startButton.addEventListener("click", (event) => {
+    verifyArrayInput(elementsInput.value)
+    setSpeed(speedInput.value);
+    startButton.classList.add("disabled");
+    stopButton.classList.remove("disabled");
+    for (let i = 0; i < buttonContainer.children.length; i++) {
+      let childButton = buttonContainer.children[i];
+      childButton.classList.add("disabled");
     }
-  }
-  return true;
-}
-// fisher-yates
-async function shuffle(array) {
-  var m = array.length, t, i;
-
-  // While there remain elements to shuffle…
-  while (m) {
-
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-
-    // And swap it with the current element.
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
-  }
-
-  return array;
-}
-
-function wait(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+    startSort();
+  })
+  stopButton.addEventListener("click", (event) => {
+    setUpCanvas();
+    startButton.classList.remove("disabled");
+    stopButton.classList.add("disabled");
+    for (let i = 0; i < buttonContainer.children.length; i++) {
+      let childButton = buttonContainer.children[i];
+      childButton.classList.remove("disabled");
+    }
+  })
+  /** end of input handler */
+});
